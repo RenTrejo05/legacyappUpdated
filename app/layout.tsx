ce95280d8105cd41137d917f71063ff0b4fc792e
RenTrejo05 <renetrejo12345@gmail.com>
2026-02-03 18:12:23 -0600

    fix: add dns-prefetch and preconnect links in RootLayout for improved font loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,12 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning lang="es">
-      <head />
+      <head>
+        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </head>
       <body
         className={clsx(
           "min-h-screen text-foreground bg-background font-sans antialiased",
