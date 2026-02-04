7a5834433106315961a84ef3c1b62a23bfd261ed
RenTrejo05 <renetrejo12345@gmail.com>
2026-02-03 18:21:57 -0600

    fix: add headers configuration to include X-Robots-Tag for SEO directives

--- next.config.js
+++ next.config.js
@@ -15,6 +15,19 @@ const nextConfig = {
     }
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
